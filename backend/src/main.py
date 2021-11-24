c961bb933085f0f77a3806c234b86cbc9dce1ef8
brownben <9870007+brownben@users.noreply.github.com>
2021-11-24 15:00:07 +0000

    feat: Single Sign On login with Google
    Add social sign in with Google using the authlib python library (https://docs.authlib.org/en/latest/index.html).
    
    Instructions for how to set up with credentials are in the README.
    
    There is currently no testing, as I don't know the best way to mock the external server needed as part of the handshake process.

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,8 +2,9 @@ import os
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from starlette.middleware.sessions import SessionMiddleware
 
+from .routes.sso import router as sso_router
 from .routes.users import router as user_router
 
 app = FastAPI(
@@ -19,10 +20,11 @@ app.add_middleware(
     allow_headers=["*"],
     allow_credentials=True,
 )
+app.add_middleware(SessionMiddleware, secret_key="some-random-string")
 
 
 # Set database config
 os.environ["PICCOLO_CONF"] = "src.database_conf"
 
-
+app.include_router(sso_router)
 app.include_router(user_router)
