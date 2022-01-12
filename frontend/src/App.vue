662238194307b0d54c2fb880805b1a7d60fb53e7
brownben <9870007+brownben@users.noreply.github.com>
2022-01-12 12:03:15 +0000

    feat: add suspense around router view
    Add suspense around the router view to allow top level data fetching with `await` in pages. Also adds page transitions.
    
    Based on the example from the Vue docs here: https://v3.vuejs.org/guide/migration/suspense.html#combining-with-other-components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,20 @@
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <template v-if="Component">
+      <transition mode="out-in">
+        <keep-alive>
+          <suspense>
+            <component :is="Component"></component>
+            <template #fallback>
+              <div>Loading...</div>
+            </template>
+          </suspense>
+        </keep-alive>
+      </transition>
+    </template>
+  </router-view>
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue';
 
