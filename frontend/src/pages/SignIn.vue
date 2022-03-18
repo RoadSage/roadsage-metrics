1b39aa69d788ca0023d2fdeb829a903ff123f97d
brownben <9870007+brownben@users.noreply.github.com>
2022-03-18 14:16:18 +0000

    feat: add image to sign in page

--- frontend/src/pages/SignIn.vue
+++ frontend/src/pages/SignIn.vue
@@ -1,6 +1,8 @@
 <template>
   <q-page class="row items-center justify-evenly constrain">
-    <div class="col-6"></div>
+    <div class="col-6">
+      <q-img src="~src\assets\landingCardThree.webp"></q-img>
+    </div>
     <SignInForm />
   </q-page>
 </template>
